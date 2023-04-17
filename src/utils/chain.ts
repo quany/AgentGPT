29d2aaefeae6aa441eb16c7abcf159be19c3a520
Quany <quany@live.com>
2023-04-17 20:46:01 +0800

    Update chain.ts

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,3 +1,104 @@
+// import { OpenAI } from "langchain/llms/openai";
+// import { PromptTemplate } from "langchain/prompts";
+// import { LLMChain } from "langchain/chains";
+// import type { ModelSettings } from "./types";
+// import { GPT_35_TURBO } from "./constants";
+
+// export const createModel = (settings: ModelSettings) =>
+//   new OpenAI({
+//     openAIApiKey:
+//       settings.customApiKey === ""
+//         ? process.env.OPENAI_API_KEY
+//         : settings.customApiKey,
+//     temperature: settings.customTemperature || 0.9,
+//     modelName:
+//       settings.customModelName === "" ? GPT_35_TURBO : settings.customModelName,
+//     maxTokens: 750,
+//   });
+
+// const startGoalPrompt = new PromptTemplate({
+//   template:
+//     "你是一个名为 AgentGPT 的自主任务创建 AI。 您有以下目标 `{goal}`。 创建一个包含 0 到 3 个任务的列表，让你的 AI 系统完成，以便更接近或完全达到你的目标。 将响应作为可在 JSON.parse() 中使用的字符串数组返回",
+//   inputVariables: ["goal"],
+// });
+// export const startGoalAgent = async (model: OpenAI, goal: string) => {
+//   return await new LLMChain({
+//     llm: model,
+//     prompt: startGoalPrompt,
+//   }).call({
+//     goal,
+//   });
+// };
+
+// const executeTaskPrompt = new PromptTemplate({
+//   template:
+//     "你是一个名为 AgentGPT 的自主任务执行 AI。 您有以下目标 `{goal}`。 您有以下任务 `{task}`。 执行任务并以字符串形式返回响应。",
+//   inputVariables: ["goal", "task"],
+// });
+// export const executeTaskAgent = async (
+//   model: OpenAI,
+//   goal: string,
+//   task: string
+// ) => {
+//   return await new LLMChain({ llm: model, prompt: executeTaskPrompt }).call({
+//     goal,
+//     task,
+//   });
+// };
+
+// const createTaskPrompt = new PromptTemplate({
+//   template:
+//     "您是 AI 任务创建代理。 您有以下目标 `{goal}`。 您有以下未完成的任务`{task}`并且刚刚执行了以下任务`{lastTask}`并收到以下结果`{result}`。 在此基础上，创建一个新任务，仅在需要时由您的 AI 系统完成，以便更接近或完全达到您的目标。 将响应作为可在 JSON.parse() 和 NOTHING ELSE 中使用的字符串数组返回",
+//   inputVariables: ["goal", "tasks", "lastTask", "result"],
+// });
+// export const executeCreateTaskAgent = async (
+//   model: OpenAI,
+//   goal: string,
+//   tasks: string[],
+//   lastTask: string,
+//   result: string
+// ) => {
+//   return await new LLMChain({ llm: model, prompt: createTaskPrompt }).call({
+//     goal,
+//     tasks,
+//     lastTask,
+//     result,
+//   });
+// };
+
+// export const extractArray = (inputStr: string): string[] => {
+//   // Match an outer array of strings (including nested arrays)
+//   const regex = /(\[(?:\s*"(?:[^"\\]|\\.)*"\s*,?)+\s*\])/;
+//   const match = inputStr.match(regex);
+
+//   if (match && match[0]) {
+//     try {
+//       // Parse the matched string to get the array
+//       return JSON.parse(match[0]) as string[];
+//     } catch (error) {
+//       console.error("Error parsing the matched array:", error);
+//     }
+//   }
+
+//   console.warn("Error, could not extract array from inputString:", inputStr);
+//   return [];
+// };
+
+// // Model will return tasks such as "No tasks added". We should filter these
+// export const realTasksFilter = (input: string): boolean => {
+//   const noTaskRegex =
+//     /^No( (new|further|additional|extra|other))? tasks? (is )?(required|needed|added|created|inputted).*$/i;
+//   const taskCompleteRegex =
+//     /^Task (complete|completed|finished|done|over|success).*/i;
+//   const doNothingRegex = /^(\s*|Do nothing(\s.*)?)$/i;
+
+//   return (
+//     !noTaskRegex.test(input) &&
+//     !taskCompleteRegex.test(input) &&
+//     !doNothingRegex.test(input)
+//   );
+// };
+
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
 import { LLMChain } from "langchain/chains";
@@ -18,7 +119,7 @@ export const createModel = (settings: ModelSettings) =>
 
 const startGoalPrompt = new PromptTemplate({
   template:
-    "你是一个名为 AgentGPT 的自主任务创建 AI。 您有以下目标 `{goal}`。 创建一个包含 0 到 3 个任务的列表，让你的 AI 系统完成，以便更接近或完全达到你的目标。 将响应作为可在 JSON.parse() 中使用的字符串数组返回",
+    "You are an autonomous task creation AI called AgentGPT. You have the following objective `{goal}`. Create a list of zero to three tasks to be completed by your AI system such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse()",
   inputVariables: ["goal"],
 });
 export const startGoalAgent = async (model: OpenAI, goal: string) => {
@@ -32,7 +133,7 @@ export const startGoalAgent = async (model: OpenAI, goal: string) => {
 
 const executeTaskPrompt = new PromptTemplate({
   template:
-    "你是一个名为 AgentGPT 的自主任务执行 AI。 您有以下目标 `{goal}`。 您有以下任务 `{task}`。 执行任务并以字符串形式返回响应。",
+    "You are an autonomous task execution AI called AgentGPT. You have the following objective `{goal}`. You have the following tasks `{task}`. Execute the task and return the response as a string.",
   inputVariables: ["goal", "task"],
 });
 export const executeTaskAgent = async (
@@ -48,7 +149,7 @@ export const executeTaskAgent = async (
 
 const createTaskPrompt = new PromptTemplate({
   template:
-    "您是 AI 任务创建代理。 您有以下目标 `{goal}`。 您有以下未完成的任务`{task}`并且刚刚执行了以下任务`{lastTask}`并收到以下结果`{result}`。 在此基础上，创建一个新任务，仅在需要时由您的 AI 系统完成，以便更接近或完全达到您的目标。 将响应作为可在 JSON.parse() 和 NOTHING ELSE 中使用的字符串数组返回",
+    "You are an AI task creation agent. You have the following objective `{goal}`. You have the following incomplete tasks `{tasks}` and have just executed the following task `{lastTask}` and received the following result `{result}`. Based on this, create a new task to be completed by your AI system ONLY IF NEEDED such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse() and NOTHING ELSE",
   inputVariables: ["goal", "tasks", "lastTask", "result"],
 });
 export const executeCreateTaskAgent = async (
